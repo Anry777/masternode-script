e8e3c920badb09678e90a050587291794011ea7a
Anry777 <raa@bancorp.ru>
2018-05-05 22:35:00 +0300

    444

--- Trittium.py
+++ Trittium.py
@@ -161,7 +161,7 @@ def download_wallet():
     
     print_info("Installing wallet dependencies...")
     os.system('apt-get install git mc build-essential libssl-dev libdb4.8-dev libdb4.8++-dev libboost1.58-all-dev libcrypto++-dev libqrencode-dev libminiupnpc-dev libgmp-dev libgmp3-dev libtool libevent-pthreads-2.0-5 -y')
-	#run_command("apt-get --assume-yes install git build-essential libssl-dev libdb4.8-dev libdb4.8++-dev libboost1.58-all-dev libcrypto++-dev libqrencode-dev libminiupnpc-dev libgmp-dev libgmp3-dev libtool libevent-pthreads-2.0-5")
+	#run_command("apt-get --assume-yes install git mc build-essential libssl-dev libdb4.8-dev libdb4.8++-dev libboost1.58-all-dev libcrypto++-dev libqrencode-dev libminiupnpc-dev libgmp-dev libgmp3-dev libtool libevent-pthreads-2.0-5")
     is_compile = True
     if os.path.isfile('/usr/local/bin/trittiumd'):
         print_warning('Wallet already installed on the system')
